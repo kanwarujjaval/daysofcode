468f84ce8a211d8f523c7f5489dc37e49a7e16ba
Kanwar Ujjaval Singh <4216199+kanwarujjaval@users.noreply.github.com>
2020-01-25 22:36:54 +0530

    180 Rotation of matrix

---
+++ src/matrix/Rotate180.java
@@ -0,0 +1,24 @@
+package matrix;
+
+public class Rotate180 {
+
+    static int N = 3;
+
+    static void printRotatedMatrix(int mat[][]) {
+        for (int i = N - 1; i >= 0; i--) {
+            for (int j = N - 1; j >= 0; j--) {
+                System.out.print(mat[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] mat = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        printRotatedMatrix(mat);
+    }
+}
