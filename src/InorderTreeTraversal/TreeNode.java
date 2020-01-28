5271792b3e8543d566fbaa7c483c4c905bf1873b
Kanwar Ujjaval Singh <4216199+kanwarujjaval@users.noreply.github.com>
2020-01-28 23:20:00 +0530

    add tree node

---
+++ src/InorderTreeTraversal/TreeNode.java
@@ -0,0 +1,11 @@
+package InorderTreeTraversal;
+
+public class TreeNode {
+    int data;
+    TreeNode left, right;
+
+    TreeNode(int item) {
+        data = item;
+        left = right = null;
+    }
+}
