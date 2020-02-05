6e69133c20b80440f44a244418af48636c57a682
Kanwar Ujjaval Singh <4216199+kanwarujjaval@users.noreply.github.com>
2020-02-05 17:12:30 +0530

    check power of 2 by single set bit

--- src/bit/PowerOfTwo.java
+++ src/bit/PowerOfTwo.java
@@ -26,9 +26,18 @@ public class PowerOfTwo {
         return countSetBit(n) == 1;
     }
 
+    boolean isPow2(int n) {
+        // unset the only bit that is set for 2s powers
+        // n=4
+        //100(n) & 011(n-1) = 0
+        if (n == 0) return false;
+        return ((n & (n - 1)) == 0);
+    }
+
     public static void main(String[] args) {
         PowerOfTwo pow = new PowerOfTwo();
         System.out.println(pow.isPow2Naive(16));
         System.out.println(pow.isPow2BitSet(16));
+        System.out.println(pow.isPow2(256));
     }
 }
